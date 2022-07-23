c4a39726e37514c07e9e7b1cef07a0c602eb494b
araiyusuke <info.yusuke.arai@gmail.com>
2022-07-23 11:58:51 +0900

    delete: 測定項目のタイプを削除

--- LineChart/ContentView.swift
+++ LineChart/ContentView.swift
@@ -11,16 +11,16 @@ struct ContentView: View {
 
     @State private var lineChartItems: [LineChartData.Items]?
 
-    let items: LineChartData.Items
+    @State private var items: LineChartData.Items
 
     init() {
 
         var lineChartData: [LineChartData] = []
 
         for _ in 1...200 {
-                 let data = LineChartData.init(type: .measure, value: CGFloat.random(in: 40...100), datetime: .init(time: "01:00", date: "12"))
-                 lineChartData.append(data)
-             }
+            let data = LineChartData.init(value: CGFloat.random(in: 0...100), datetime: .init(time: "01:00", date: "12"))
+            lineChartData.append(data)
+        }
 
         items = LineChartData.Items(
             data: lineChartData
@@ -29,26 +29,33 @@ struct ContentView: View {
 
 
     var body: some View {
-        ZStack {
-            GeometryReader { geometry in
-                LineChart(
-                    items: items
-                )
-                .frame(
-                    maxWidth: .infinity,
-                    maxHeight: geometry.size.height
+        VStack {
+            ZStack {
+                GeometryReader { geometry in
+                    LineChart(
+                        items: items
+                    )
+                    .frame(
+                        maxWidth: .infinity,
+                        maxHeight: geometry.size.height
+                    )
+                    .background(Color.red.opacity(0.3))
+                }
+            }
+            .frame(height: 140)
+            .onTapGesture {
+                var lineChartData: [LineChartData] = []
+
+                for _ in 1...200 {
+                    let data = LineChartData.init(value: CGFloat.random(in: 0...100), datetime: .init(time: "01:00", date: "12"))
+                    lineChartData.append(data)
+                }
+
+                items = LineChartData.Items(
+                    data: lineChartData
                 )
             }
         }
-        .frame(height: 140)
-    }
-}
-
-extension LineChartData {
-    enum type {
-        case target // 目標値
-        case initial // 初回値
-        case measure // 測定値
     }
 }
 
@@ -60,7 +67,6 @@ extension LineChartData {
 }
 
 struct LineChartData  {
-    let type: type
     let value: CGFloat?
     let datetime: DateTime
 }
@@ -102,11 +108,11 @@ struct LineChart: View {
                         ZStack {
                             lineAndCircle
                             label
-                            optionLine
+//                            optionLine
                         }
                         .padding(.vertical, padding / 2)
                     }
-                    .frame(width: 2000, height: geometry.size.height)
+                    .frame(width: CGFloat(stepMaxCount * 40), height: geometry.size.height)
                 }
             }
         }
@@ -137,28 +143,28 @@ struct LineChart: View {
     }
 
     // 初回値、目標値の横軸ライン
-    private var optionLine: some View {
-
-        GeometryReader { geometry in
-
-            ZStack {
-
-                if let initialValue = items.initialValue {
-                    Border(color: Color.red)
-                        .frame(height: 2)
-                        .offset(y: initialValue)
-                    //                        .opacity(options.`init` == true ? 1 : 0)
-                }
-
-                if let targetValue = items.targetValue {
-                    Border(color: Color.blue)
-                        .frame(height: 2)
-                        .offset(y: targetValue)
-                    //                        .opacity(options.target == true ? 1 : 0)
-                }
-            }
-        }
-    }
+//    private var optionLine: some View {
+//
+//        GeometryReader { geometry in
+//
+//            ZStack {
+//
+//                if let initialValue = items.initialValue {
+//                    Border(color: Color.red)
+//                        .frame(height: 2)
+//                        .offset(y: initialValue)
+//                    //                        .opacity(options.`init` == true ? 1 : 0)
+//                }
+//
+//                if let targetValue = items.targetValue {
+//                    Border(color: Color.blue)
+//                        .frame(height: 2)
+//                        .offset(y: targetValue)
+//                    //                        .opacity(options.target == true ? 1 : 0)
+//                }
+//            }
+//        }
+//    }
 
     // グラフの縦線
     private var verticalLine: some View {
@@ -288,18 +294,14 @@ extension LineChartData {
 
         // 測定結果データ
         var measures: [CGFloat?] {
-            return data.filter {
-                $0.type == .measure
-            }.map {
+            return data.map {
                 $0.value
             }
         }
 
         // ラベルを返す(エラーの場合は空文字)
         var measureLabels: [String] {
-            return data.filter {
-                $0.type == .measure
-            }.map {
+            return data.map {
                 return  String(format: "%.1f", $0.value ?? 0)
             }
         }
@@ -332,55 +334,49 @@ extension LineChartData {
             return result
         }
 
-        func getLineChartData(_ types: type...) -> [LineChartData]? {
-            return data.filter {
-                types.contains($0.type)
-            }
-        }
-
-        var initialValue: CGFloat? {
-
-            if let first = getLineChartData(.initial)?.first {
-
-                guard let valueNotNill = first.value else {
-                    return nil
-                }
-
-                if valueNotNill >= max {
-                    return 0
-                }
-
-                if valueNotNill <= min {
-                    return  plotAreaHeight
-                }
-
-                return calculateYAxis(y: valueNotNill)
-
-            }
-            return nil
-        }
-
-        var targetValue: CGFloat? {
-
-            if let target = getLineChartData(.target)?.first {
-
-                guard let valueNotNill = target.value else {
-                    return nil
-                }
-
-                if valueNotNill >= max {
-                    return 0
-                }
-
-                if valueNotNill <= min {
-                    return  plotAreaHeight
-                }
-
-                return calculateYAxis(y: valueNotNill)
-
-            }
-            return nil
-        }
+//        var initialValue: CGFloat? {
+//
+//            if let first = getLineChartData(.initial)?.first {
+//
+//                guard let valueNotNill = first.value else {
+//                    return nil
+//                }
+//
+//                if valueNotNill >= max {
+//                    return 0
+//                }
+//
+//                if valueNotNill <= min {
+//                    return  plotAreaHeight
+//                }
+//
+//                return calculateYAxis(y: valueNotNill)
+//
+//            }
+//            return nil
+//        }
+//
+//        var targetValue: CGFloat? {
+//
+//            if let target = getLineChartData(.target)?.first {
+//
+//                guard let valueNotNill = target.value else {
+//                    return nil
+//                }
+//
+//                if valueNotNill >= max {
+//                    return 0
+//                }
+//
+//                if valueNotNill <= min {
+//                    return  plotAreaHeight
+//                }
+//
+//                return calculateYAxis(y: valueNotNill)
+//
+//            }
+//            return nil
+//        }
 
         func calculateYAxis(y: CGFloat) -> CGFloat {
             return plotAreaHeight - (plotAreaHeight / ( max - min) * (max - min  - (max - y)))
