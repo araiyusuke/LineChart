cb65811e0a5a9549487ff9adb6982c7c729d4cb8
araiyusuke <info.yusuke.arai@gmail.com>
2022-07-23 09:03:15 +0900

    Initial Commit

---
+++ LineChart/LineChartApp.swift
@@ -0,0 +1,17 @@
+//
+//  LineChartApp.swift
+//  LineChart
+//
+//  Created by 名前なし on 2022/07/23.
+//
+
+import SwiftUI
+
+@main
+struct LineChartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
